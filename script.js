9283c4e881d81cbc28f52e9e3afe65a30fd1109d
ryan-dfw <mckevitt.r@gmail.com>
2023-03-17 23:50:48 -0500

    kicked the can down the road of deciding what the read from list object will have become once the randomization engine has concluded its randomization

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const IMG_WIDTH = 300;
 const IMG_HEIGHT = 160;
 const FOLDER_PATH = "./assets/png/"
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
 
 function drawFlag(filename) {
-    
+    // Place image on the canvas
+    var img = new Image();
+    img.src = FOLDER_PATH + filename
+    img.width = IMG_WIDTH;
+    img.height = IMG_HEIGHT;
+    img.onload = function() {
+        context.drawImage(img, 0, 0, IMG_WIDTH, IMG_HEIGHT);
+    }    
 }
 
 $(document).ready(function(){
     console.log("Hello World!");
 
-    var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("2d");
+
 
     // Clear the canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    var filename = FOLDER_PATH + "ad.png"
 
-    // Place image on the canvas
-    var img = new Image();
-    img.src = filename
+    var filename = "ad.png"
 
-    img.width = IMG_WIDTH;
-    img.height = IMG_HEIGHT;
-
-    img.onload = function() {
-        context.drawImage(img, 0, 0, IMG_WIDTH, IMG_HEIGHT);
-    }
+    drawFlag(filename)
 });
