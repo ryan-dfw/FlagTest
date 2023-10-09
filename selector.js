fc21c8e538736048ea9ceede9714a99cb26506f9
gdknas <mckevitt.r@gmail.com>
2023-10-09 17:59:59 -0500

    recreated homepage buttons with bootstrap as radio

--- selector.js
+++ selector.js
@@ -1,15 +1,15 @@
+var button5 = document.getElementById("select5Btn");
 var button20 = document.getElementById("select20Btn");
-var button50 = document.getElementById("select50Btn");
 var button100 = document.getElementById("select100Btn");
 var buttonAll = document.getElementById("selectAllBtn");
 
 var numFlags;
 
+button5.addEventListener("click", function() {
+    numFlags = 5;
+});
 button20.addEventListener("click", function() {
     numFlags = 20;
-});
-button50.addEventListener("click", function() {
-    numFlags = 50;
 });
 button100.addEventListener("click", function() {
     numFlags = 100;
